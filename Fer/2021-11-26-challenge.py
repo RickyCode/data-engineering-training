914beaf6306de5b3499244e4709f332b5ea96b0d
Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-12-03 17:01:20 -0300

    Cambios de nombre de los archivos y lectura del archivo json

--- Fer/2021-11-26-challenge.py
+++ Fer/2021-11-26-challenge.py
@@ -1,7 +1,7 @@
 import json
 import boto3
-import csv
-import pandas as pd
+
+
 
 keycsv ='datalake/ingestion-layer/raw-zone/2021-11-26-challenge/internet_usage_by_countries.csv'
 keyjson='datalake/ingestion-layer/raw-zone/2021-11-26-challenge/internet_usage_by_countries.json'
@@ -9,17 +9,18 @@ keyparquet='datalake/ingestion-layer/raw-zone/2021-11-26-challenge/internet_usag
 bucket ='data-engineering-training'
 def lambda_handler(event,context):
     ##leyendo el archivo csv
-    s3_resource = boto3.resource('s3')
-         
+  
     s3_client=boto3.client('s3')
     csvfile = s3_client.get_object(Bucket = bucket, Key = keycsv)
     csvcontent = csvfile['Body'].read().decode('utf-8')
- 
     print("csv",csvcontent)
     
     ##Leyendo el json
-    s3json= boto3.resource('s3')
-    content_object=s3json.Object(bucket,keyjson)
-    ##file_content = content_object.get()['Body'].read().decode('utf-8')
-    json_content = json.load(content_objec.get()['Body'])
-    print(json_content)
+    s3= boto3.resource('s3')
+    content_object=s3.Object(bucket,keyjson)
+    file_content = content_object.get()['Body'].read().decode('utf-8').split()
+
+    print(file_content)
+    
+  
+   
