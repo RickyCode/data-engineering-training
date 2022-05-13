87fba8ced831d5c4e1a0832c3be6841c4436ab52
ricky_kranio <ricardo@kranio.io>
2022-05-13 14:00:50 -0400

    add serverless framework demo

---
+++ serverless-framework-demo/handler.py
@@ -0,0 +1,13 @@
+import json
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "event": event,
+        "context": context,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
