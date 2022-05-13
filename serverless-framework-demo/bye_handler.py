781c77f9f6ff2996efd2cd3733f5fd5c6b1c8e2a
ricky_kranio <ricardo@kranio.io>
2022-05-13 17:25:04 -0400

    bye function update

--- serverless-framework-demo/bye_handler.py
+++ serverless-framework-demo/bye_handler.py
@@ -7,7 +7,7 @@ def add_group_name():
 
 def bye(event, context):
     body = {
-        "message": f"Bye, good weekend {add_group_name()} team!",
+        "message": f"Bye, good weekend {add_group_name()} team! Good job!",
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
